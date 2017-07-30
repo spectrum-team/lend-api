e725491263ce826596d115156348fa9987eb0ca9
Gustavo Moya <gmoyatorres@gmail.com>
2017-07-29 23:04:29 -0400

    Completing the Assets Controller. Will follow up with the other ones.

--- controllers/asset.go
+++ controllers/asset.go
@@ -50,6 +50,43 @@ func (a *AssetController) FindByID(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(asset)
 }
 
+// Find receives a JSON containing the query to be made to the database
+func (a *AssetController) Find(w http.ResponseWriter, r *http.Request) {
+	var req interface{}
+	errResponse := &models.ErrorResponse{}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		errResponse.ApplicationMessage = err.Error()
+		errResponse.UserMessage = "Error parsing json"
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(errResponse)
+		return
+	}
+
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		errResponse.ApplicationMessage = err.Error()
+		errResponse.UserMessage = "Error parsing json"
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(errResponse)
+		return
+	}
+
+	assets, err := a.repo.Find(req)
+	if err != nil {
+		errResponse.ApplicationMessage = err.Error()
+		errResponse.UserMessage = "Error trying to find assets"
+		w.WriteHeader(500)
+		json.NewEncoder(w).Encode(errResponse)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(assets)
+}
+
 // Insert creates a new Asset
 func (a *AssetController) Insert(w http.ResponseWriter, r *http.Request) {
 	req := &models.Asset{}
@@ -77,6 +114,54 @@ func (a *AssetController) Insert(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		errResponse.ApplicationMessage = err.Error()
 		errResponse.UserMessage = "Error trying to create the asset"
+		w.WriteHeader(500)
+		json.NewEncoder(w).Encode(errResponse)
+		return
+	}
+
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode("Ok")
+}
+
+// Update refreshes an asset using the ID passed in the URL and the json body
+func (a *AssetController) Update(w http.ResponseWriter, r *http.Request) {
+	req := &models.Asset{}
+	errResponse := &models.ErrorResponse{}
+	vars := mux.Vars(r)
+
+	id := vars["id"]
+	if id == "" {
+		errResponse.ApplicationMessage = ""
+		errResponse.UserMessage = "ID not specified"
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(errResponse)
+		return
+	}
+
+	bsonID := bson.ObjectIdHex(id)
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		errResponse.ApplicationMessage = err.Error()
+		errResponse.UserMessage = "Error parsing json"
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(errResponse)
+		return
+	}
+
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		errResponse.ApplicationMessage = err.Error()
+		errResponse.UserMessage = "Error parsing json"
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(errResponse)
+		return
+	}
+
+	err = a.repo.Update(bsonID, *req)
+	if err != nil {
+		errResponse.ApplicationMessage = err.Error()
+		errResponse.UserMessage = "Error trying to update the asset"
 		w.WriteHeader(500)
 		json.NewEncoder(w).Encode(errResponse)
 		return
